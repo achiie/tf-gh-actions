09f215a6c46e9fb3915424e9e11098ea0f9f10d3
Achyut <sharma.achyut17@gmail.com>
2021-06-21 17:24:09 +0530

    gce vm  updates

--- variables.tf
+++ variables.tf
@@ -7,3 +7,19 @@ variable "region" {
   type    = string
   default = "europe-west2"
 }
+
+variable "zones" {
+  description = "GCP zone identifier"
+  type        = list
+}
+
+variable "host_project" {
+  description = "shared vpc name"
+  type        = string
+  default     = "poc-network"
+}
+
+variable "subnetwork" {
+  description = "Subnet to deploy to. Only one of network or subnetwork should be specified."
+  default     = ""
+}
