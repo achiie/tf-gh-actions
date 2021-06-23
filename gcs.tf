0b63e81fc7dfe3e0b54944095e8f461bc8272b61
Achyut <sharma.achyut17@gmail.com>
2021-06-23 16:43:17 +0530

    Config Validation Events

--- gcs.tf
+++ gcs.tf
@@ -2,6 +2,6 @@ resource "google_storage_bucket" "bucket" {
   name = "test-bucket-gh-actions-202555"
 }
 
-resource "google_storage_bucket" "bucket" {
+resource "google_storage_bucket" "bucket_2" {
   name = "test-bucket-gh-actions-202555121"
 }
