955070438f0753aca14acf221e05b52514b15b98
Achyut <sharma.achyut17@gmail.com>
2021-06-23 18:13:32 +0530

    VM addition Events

--- gcs.tf
+++ gcs.tf
@@ -1,7 +1,3 @@
 resource "google_storage_bucket" "bucket" {
   name = "test-bucket-gh-actions-202555"
 }
-
-resource "google_storage_bucket" "bucket_2" {
-  name = "test-bucket-gh-actions-202555121"
-}
