773007ae1a1aa539940b4876d6c23ebbf9d831a1
Achyut <sharma.achyut17@gmail.com>
2021-06-28 14:03:31 +0530

    Deleting the Infrastructure

--- disks.tf
+++ disks.tf
@@ -1,9 +1,9 @@
-resource "google_compute_disk" "poc_boot" {
-  name  = "poc-boot"
-  zone  = var.zones[0]
-  image = "ubuntu-2004-lts"
-  size  = 10
-  type  = "pd-ssd"
+# resource "google_compute_disk" "poc_boot" {
+#   name  = "poc-boot"
+#   zone  = var.zones[0]
+#   image = "ubuntu-2004-lts"
+#   size  = 10
+#   type  = "pd-ssd"
 
-  physical_block_size_bytes = 4096
-}
+#   physical_block_size_bytes = 4096
+# }
