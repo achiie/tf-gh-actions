9402ccdbf7f1dd0585cc3cd10e20b44cd187ef79
Achyut <sharma.achyut17@gmail.com>
2021-06-29 17:18:17 +0530

    VPC CIDR update

--- vpc.tf
+++ vpc.tf
@@ -18,10 +18,3 @@ resource "google_compute_subnetwork" "subnet_work" {
     ip_cidr_range = "10.25.3.0/24"
   }
 }
-
-resource "google_compute_subnetwork" "subnet_work_2" {
-  name          = "subnet-2"
-  ip_cidr_range = "10.2.0.1/16"
-  region        = "europe-west2"
-  network       = google_compute_network.vpc_network.id
-}
